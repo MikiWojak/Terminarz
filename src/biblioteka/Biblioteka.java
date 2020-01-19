80725dd90c92cd4506a32fa3fa4a5c45d4a579dc
MikZarZSG <mikzarzsg@gmail.com>
2020-01-19 17:21:45 +0100

    Klasa Biblioteka
    Pola i konstruktor

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -1,5 +1,41 @@
 package biblioteka;
 
-public class Biblioteka {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import model.Czytelnik;
+import model.Ksiazka;
+import model.Wypozyczenia;
+
+public class Biblioteka {
+	
+	public static final String DRIVER = "org.sqlite.JDBC";
+	public static final String DB_URL = "jdbc:sqlite:biblioteka.db";
+	
+	private Connection conn;
+	private Statement stat;
+	
+	public Biblioteka() {
+		//za³adowanie sterownika
+		try {
+			Class.forName(Biblioteka.DRIVER);
+		} catch (ClassNotFoundException e) {
+			System.err.println("Brak sterownika JDBC!");
+			e.printStackTrace();
+		}
+		
+		//otwarcie po³¹czenia
+		try {
+			conn = DriverManager.getConnection(DB_URL);
+			stat = conn.createStatement();
+		} catch (SQLException e) {
+			System.err.println("Problem z otwarciem po³¹czenia!");
+			e.printStackTrace();
+		}
+		
+		//tworzenie tabel
+		create_tables();
+	}
 }
