001de206513e6a4c14a778740d296ef3d0350fe4
MikZarZSG <mikzarzsg@gmail.com>
2020-01-19 14:30:09 +0100

    Metody klasy Wypozyczenia
    Metody dla zmiennej id_wypozyczenia

--- src/model/Wypozyczenia.java
+++ src/model/Wypozyczenia.java
@@ -5,4 +5,8 @@ public class Wypozyczenia {
 	private int id_wypozyczenia;
 	private int id_ksiazki;
 	private int id_czytelnika;
+	
+	//operacje na danych
+	private int get_id_wypozyczenia() { return id_wypozyczenia; }
+	private void set_id_wypozyczenia(int id_wypozyczenia) { this.id_wypozyczenia = id_wypozyczenia; }
 }
