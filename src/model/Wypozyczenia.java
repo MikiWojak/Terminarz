c5c06b80df5813a0f3c1ec727cdfa97ddd33a095
MikZarZSG <mikzarzsg@gmail.com>
2020-01-19 17:12:03 +0100

    Klasa Wypozyczenia
    Uzupelniono metody

--- src/model/Wypozyczenia.java
+++ src/model/Wypozyczenia.java
@@ -7,6 +7,20 @@ public class Wypozyczenia {
 	private int id_czytelnika;
 	
 	//operacje na danych
-	private int get_id_wypozyczenia() { return id_wypozyczenia; }
-	private void set_id_wypozyczenia(int id_wypozyczenia) { this.id_wypozyczenia = id_wypozyczenia; }
+	public int get_id_wypozyczenia() { return id_wypozyczenia; }
+	public void set_id_wypozyczenia(int id_wypozyczenia) { this.id_wypozyczenia = id_wypozyczenia; }
+	
+	public int get_id_ksiazki() { return id_ksiazki; }
+	public void set_id_ksiazki(int id_ksiazki) { this.id_ksiazki = id_ksiazki; }
+	
+	public int get_id_czytelnika() { return id_czytelnika; }
+	public void set_id_czytelnika(int id_czytelnika) { this.id_czytelnika = id_czytelnika; }
+	
+	//konstruktory
+	public Wypozyczenia() {}
+	public Wypozyczenia(int id_wypozyczenia, int id_ksiazki, int id_czytelnika) {
+		this.id_wypozyczenia = id_wypozyczenia;
+		this.id_ksiazki = id_ksiazki;
+		this.id_czytelnika = id_czytelnika;
+	}
 }
