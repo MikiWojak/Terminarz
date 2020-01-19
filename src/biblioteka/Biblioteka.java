3b349bd3b0d1d02081b1992e9eb0c0d08cc002e3
MikZarZSG <mikzarzsg@gmail.com>
2020-01-19 17:33:18 +0100

    Klasa Biblioteka
    Metoda do twotzenia tabel
    Dodano komentarze

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -10,13 +10,14 @@ import model.Ksiazka;
 import model.Wypozyczenia;
 
 public class Biblioteka {
-	
+	//pola
 	public static final String DRIVER = "org.sqlite.JDBC";
 	public static final String DB_URL = "jdbc:sqlite:biblioteka.db";
 	
 	private Connection conn;
 	private Statement stat;
 	
+	//konstruktor
 	public Biblioteka() {
 		//za³adowanie sterownika
 		try {
@@ -37,5 +38,35 @@ public class Biblioteka {
 		
 		//tworzenie tabel
 		create_tables();
+	}
+	
+	//tworzenie tabel
+	private boolean create_tables() {
+		String create_czytelnicy = "CREATE TABLE IF NOT EXISTS czytelnicy"
+				+ "(id_czytelnika INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ "imie VARCHAR(255),"
+				+ "nazwisko VARCHAR(255),"
+				+ "pesel VARCHAR(255))";
+		String create_ksiazki = "CREATE TABLE IF NOT EXISTS ksiazki"
+				+ "(id_ksiazki INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ "tytul VARCHAR(255),"
+				+ "autor VARCHAR(255))";
+		String create_wypozyczenia = "CREATE TABLE IF NOT EXISTS wypozyczenia"
+				+ "(id_wypozyczenia INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ "id_czytelnika INT,"
+				+ "id_ksiazki INT)";
+		
+		try {
+			stat.execute(create_czytelnicy);
+			stat.execute(create_ksiazki);
+			stat.execute(create_wypozyczenia);
+		} catch (SQLException e) {
+			System.err.println("B³¹d przy tworzeniu tabeli!");
+			e.printStackTrace();
+			
+			return false;
+		}
+		
+		return true;
 	}
 }
