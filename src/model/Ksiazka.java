da7e3b1c82f255e29ec3dcc022732430a1b10b39
MikZarZSG <mikzarzsg@gmail.com>
2020-01-19 14:16:35 +0100

    Klasa Ksiazka
    Uzupelnienie klasy:
    - pola
    - metody

--- src/model/Ksiazka.java
+++ src/model/Ksiazka.java
@@ -1,5 +1,30 @@
 package model;
 
 public class Ksiazka {
-
+	private int id;
+	private String tytul;
+	private String autor;
+	
+	//operacje na danych
+	public int get_id() { return id; }
+	public void set_id(int id) { this.id = id; }
+	
+	public String get_tytul() { return tytul; }
+	public void set_tytul(String tytul) { this.tytul = tytul; }
+	
+	public String get_autor() { return autor; }
+	public void set_autor(String autor) { this.autor = autor; }
+	
+	//konstruktory
+	public Ksiazka (int id, String tytul, String autor) {
+		this.id = id;
+		this.tytul = tytul;
+		this.autor = autor;
+	}
+	
+	//wyœwietlanie
+	public String toString() {
+		return id + "\t" + tytul + "\t" + autor;
+	}
+		
 }
