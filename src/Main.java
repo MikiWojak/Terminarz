0804006e772e888acfae5a7035ce7e2d185e4927
MikZarZSG <mikzarzsg@gmail.com>
2020-01-22 17:08:48 +0100

    Test BD
    Wywolanie BD w funkcji glownej
    Na razie dziala

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import java.awt.EventQueue;
+import java.util.List;
+
+import biblioteka.Biblioteka;
+import model.Czytelnik;
+import model.Ksiazka;
+import model.Wypozyczenia;
 
 public class Main {
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		/*
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -14,5 +21,43 @@ public class Main {
 				}
 			}
 		});
+		*/
+		
+		//TEST BD
+		
+		//otwarcie po³¹czenia
+		Biblioteka biblioteka = new Biblioteka();
+		
+		//dodanie czytelników
+		biblioteka.insert_czytelnik("Mikolaj", "Zarnowski", "01234567890");
+		biblioteka.insert_czytelnik("Jan", "Kowalski", "09876543210");
+		biblioteka.insert_czytelnik("Pawel", "Nowak", "13579246801");
+		
+		//dodanie ksiazek
+		biblioteka.insert_ksiazka("Wiedzmin", "Andrzej Sapkowski");
+		biblioteka.insert_ksiazka("Wladca Pierscieni", "John R.R. Tolkien");
+		biblioteka.insert_ksiazka("Sword Art Online", "Reki Kawahara");
+		
+		//listy czytelników i ksi¹¿ek
+		List<Czytelnik>czytelnicy = biblioteka.select_czytelnicy();
+		List<Ksiazka>ksiazki = biblioteka.select_ksiazki();
+		
+		//wyswietlenie
+		System.out.println("Lista czytelników:");
+		//skrócona wersja FOR dla ka¿dego elementu listy
+		for(Czytelnik c : czytelnicy)
+		{
+			System.out.println(c);
+		}
+		
+		System.out.println("Lista ksi¹¿ek:");
+		//skrócona wersja FOR dla ka¿dego elementu listy
+		for(Ksiazka k : ksiazki)
+		{
+			System.out.println(k);
+		}
+		
+		//zamkniêcie po³¹czenia
+		biblioteka.close_connection();
 	}
 }
