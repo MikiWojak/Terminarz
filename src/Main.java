1e6502256ade9efe55eaa5eeaa624f22ac38be5f
MikiWojak <mikizarowa8@gmail.com>
2020-01-23 17:22:56 +0100

    Usuniecie komentarzy

--- src/Main.java
+++ src/Main.java
@@ -11,34 +11,10 @@ public class Main {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		/*
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Program window = new Program();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		*/
-		
 		//TEST BD
 		
 		//otwarcie po³¹czenia
 		Biblioteka biblioteka = new Biblioteka();
-		
-		/*
-		//dodanie czytelników
-		biblioteka.insert_czytelnik("Mikolaj", "Zarnowski", "01234567890");
-		biblioteka.insert_czytelnik("Jan", "Kowalski", "09876543210");
-		biblioteka.insert_czytelnik("Pawel", "Nowak", "13579246801");
-		
-		//dodanie ksiazek
-		biblioteka.insert_ksiazka("Wiedzmin", "Andrzej Sapkowski");
-		biblioteka.insert_ksiazka("Wladca Pierscieni", "John R.R. Tolkien");
-		biblioteka.insert_ksiazka("Sword Art Online", "Reki Kawahara");
-		*/
 		
 		//listy czytelników i ksi¹¿ek
 		List<Czytelnik>czytelnicy = biblioteka.select_czytelnicy();
