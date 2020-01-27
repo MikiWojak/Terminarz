36ca40b4906b50bf75d71ad153e9fac779efb7b6
MikiWojak <mikizarowa8@gmail.com>
2020-01-27 15:21:05 +0100

    Nowa BD z datami
    Konwersja ze Stringa na Date
    W BD data jest w formie liczbowej

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.awt.EventQueue;
+import java.sql.Date;
 import java.util.List;
 
 import biblioteka.Biblioteka;
@@ -11,34 +12,8 @@ public class Main {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		/*
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Program window = new Program();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		*/
-		
-		//TEST BD
-		
 		//otwarcie po³¹czenia
 		Biblioteka biblioteka = new Biblioteka();
-		
-		/*
-		//dodanie czytelników
-		biblioteka.insert_czytelnik("Mikolaj", "Zarnowski", "01234567890");
-		biblioteka.insert_czytelnik("Jan", "Kowalski", "09876543210");
-		biblioteka.insert_czytelnik("Pawel", "Nowak", "13579246801");
-		
-		//dodanie ksiazek
-		biblioteka.insert_ksiazka("Wiedzmin", "Andrzej Sapkowski");
-		biblioteka.insert_ksiazka("Wladca Pierscieni", "John R.R. Tolkien");
-		biblioteka.insert_ksiazka("Sword Art Online", "Reki Kawahara");
-		*/
 		
 		//listy czytelników i ksi¹¿ek
 		List<Czytelnik>czytelnicy = biblioteka.select_czytelnicy();
