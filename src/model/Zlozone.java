dc6d95013074235d6d21a1d921e4da0c1c195d5f
MikiWojak <mikizarowa8@gmail.com>
2020-02-02 10:40:43 +0100

    Klasa Zlozone napisana
    Dodano konstruktory i metody
    NIestarannie napisana klasa - powtarzalnosc kodu
    Moze interfejsy cos dadza

--- src/model/Zlozone.java
+++ src/model/Zlozone.java
@@ -15,4 +15,47 @@ public class Zlozone {
 	private int id_grupa;
 	private String nazwa_grupa;
 	private String opis_grupa;
+	
+	public Zlozone() {}
+	public Zlozone(int id_zadanie, Date data_zadanie, String tytul_zadanie, String opis_zadanie, String priorytet_zadanie, boolean czy_wykonane, int id_grupa, String nazwa_grupa, String opis_grupa) {
+		this.id_zadanie = id_zadanie;
+		this.data_zadanie = data_zadanie;
+		this.tytul_zadanie = tytul_zadanie;
+		this.opis_zadanie = opis_zadanie;
+		this.priorytet_zadanie = priorytet_zadanie;
+		this.czy_wykonane = czy_wykonane;
+		
+		this.id_grupa = id_grupa;
+		this.nazwa_grupa = nazwa_grupa;
+		this.opis_grupa = opis_grupa;
+	}
+	
+	//obs³uga danych
+	public int pobierz_id_zadanie() { return id_zadanie; }
+	public void ustaw_id_zadanie(int id_zadanie) { this.id_zadanie = id_zadanie; }
+		
+	public Date pobierz_data_zadanie() { return data_zadanie; }
+	public void ustaw_data_zadanie(Date data_zadanie) { this.data_zadanie = data_zadanie; }
+		
+	public String pobierz_tytul_zadanie() { return tytul_zadanie; }
+	public void ustaw_tytul_zadanie(String tytul_zadanie) { this.tytul_zadanie = tytul_zadanie; }
+		
+	public String pobierz_opis_zadanie() { return opis_zadanie; }
+	public void ustaw_opis_zadanie(String opis_zadanie) { this.opis_zadanie = opis_zadanie; }
+		
+	public String pobierz_priorytet_zadanie() { return priorytet_zadanie; }
+	public void ustaw_priorytet_zadanie(String priorytet_zadanie) { this.priorytet_zadanie = priorytet_zadanie; }
+		
+	public boolean pobierz_czy_wykonane() { return czy_wykonane; }
+	public void ustaw_czy_wykonane(boolean czy_wykonane) { this.czy_wykonane = czy_wykonane; }
+	
+	//obs³uga danych
+	public int pobierz_id_grupa() { return id_grupa; }
+	public void ustaw_id_grupa(int id_grupa) { this.id_grupa = id_grupa; }
+		
+	public String pobierz_nazwa_grupa() { return nazwa_grupa; }
+	public void ustaw_nazwa_grupa(String nazwa_grupa) { this.nazwa_grupa = nazwa_grupa; }
+		
+	public String pobierz_opis_grupa() { return opis_grupa; }
+	public void ustaw_opis_grupa(String opis_grupa) { this.opis_grupa = opis_grupa; }
 }
