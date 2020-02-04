bd4144124e6d1db8f3895710ffc6526faa60d5b2
MikiWojak <mikizarowa8@gmail.com>
2020-02-04 11:35:48 +0100

    Przygotowanie interfejsu graficznego
    Dodano element wyswietlajacy liste
    Na niej bedzie sie wyswietlac: data i tytul

--- src/Program.java
+++ src/Program.java
@@ -16,6 +16,7 @@ import java.sql.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.awt.event.ActionEvent;
+import javax.swing.JList;
 
 public class Program {
 	
@@ -34,13 +35,14 @@ public class Program {
 	private JLabel lbl_priorytet_zadanie;
 	private JLabel lbl_czy_wykonane;
 	private JButton Aktualizuj;
+	private JList lista;
 
 	/**
 	 * Create the application.
 	 */
 	public Program() {
-		initialize();
 		inicjalizacja_pol();
+		initialize();
 	}
 
 	/**
@@ -48,43 +50,43 @@ public class Program {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 1111, 592);
+		frame.setBounds(100, 100, 1111, 824);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		lbl_nazwa_grupa = new JLabel("New label");
-		lbl_nazwa_grupa.setFont(new Font("Arial", Font.PLAIN, 24));
-		lbl_nazwa_grupa.setBounds(12, 13, 1069, 37);
+		lbl_nazwa_grupa.setFont(new Font("Arial", Font.PLAIN, 20));
+		lbl_nazwa_grupa.setBounds(24, 436, 1069, 30);
 		frame.getContentPane().add(lbl_nazwa_grupa);
 		
 		lbl_opis_grupa = new JLabel("New label");
-		lbl_opis_grupa.setFont(new Font("Arial", Font.PLAIN, 24));
-		lbl_opis_grupa.setBounds(12, 63, 1069, 37);
+		lbl_opis_grupa.setFont(new Font("Arial", Font.PLAIN, 20));
+		lbl_opis_grupa.setBounds(24, 479, 1069, 30);
 		frame.getContentPane().add(lbl_opis_grupa);
 		
 		lbl_tytul_zadanie = new JLabel("New label");
-		lbl_tytul_zadanie.setFont(new Font("Arial", Font.PLAIN, 24));
-		lbl_tytul_zadanie.setBounds(12, 113, 1069, 37);
+		lbl_tytul_zadanie.setFont(new Font("Arial", Font.PLAIN, 20));
+		lbl_tytul_zadanie.setBounds(24, 522, 1069, 30);
 		frame.getContentPane().add(lbl_tytul_zadanie);
 		
 		lbl_data_zadanie = new JLabel("New label");
-		lbl_data_zadanie.setFont(new Font("Arial", Font.PLAIN, 24));
-		lbl_data_zadanie.setBounds(12, 163, 1069, 37);
+		lbl_data_zadanie.setFont(new Font("Arial", Font.PLAIN, 20));
+		lbl_data_zadanie.setBounds(24, 565, 1069, 30);
 		frame.getContentPane().add(lbl_data_zadanie);
 		
 		lbl_opis_zadanie = new JLabel("New label");
-		lbl_opis_zadanie.setFont(new Font("Arial", Font.PLAIN, 24));
-		lbl_opis_zadanie.setBounds(12, 213, 1069, 37);
+		lbl_opis_zadanie.setFont(new Font("Arial", Font.PLAIN, 20));
+		lbl_opis_zadanie.setBounds(24, 608, 1069, 30);
 		frame.getContentPane().add(lbl_opis_zadanie);
 		
 		lbl_priorytet_zadanie = new JLabel("New label");
-		lbl_priorytet_zadanie.setFont(new Font("Arial", Font.PLAIN, 24));
-		lbl_priorytet_zadanie.setBounds(12, 263, 1069, 37);
+		lbl_priorytet_zadanie.setFont(new Font("Arial", Font.PLAIN, 20));
+		lbl_priorytet_zadanie.setBounds(24, 651, 1069, 30);
 		frame.getContentPane().add(lbl_priorytet_zadanie);
 		
 		lbl_czy_wykonane = new JLabel("New label");
-		lbl_czy_wykonane.setFont(new Font("Arial", Font.PLAIN, 24));
-		lbl_czy_wykonane.setBounds(12, 313, 1069, 37);
+		lbl_czy_wykonane.setFont(new Font("Arial", Font.PLAIN, 20));
+		lbl_czy_wykonane.setBounds(24, 694, 1069, 30);
 		frame.getContentPane().add(lbl_czy_wykonane);
 		
 		Aktualizuj = new JButton("Aktualizuj");
@@ -93,9 +95,13 @@ public class Program {
 				aktualizuj_dane();
 			}
 		});
-		Aktualizuj.setFont(new Font("Arial", Font.PLAIN, 24));
-		Aktualizuj.setBounds(457, 363, 222, 37);
+		Aktualizuj.setFont(new Font("Arial", Font.PLAIN, 20));
+		Aktualizuj.setBounds(871, 740, 222, 37);
 		frame.getContentPane().add(Aktualizuj);
+		
+		lista = new JList();
+		lista.setBounds(326, 13, 755, 414);
+		frame.getContentPane().add(lista);
 		
 		frame.setVisible(true);
 	}
