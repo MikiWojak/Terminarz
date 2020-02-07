ed5395603135abbcd9e3041f97c67acc3b09c0e4
MikiWojak <mikizarowa8@gmail.com>
2020-02-07 10:58:54 +0100

    Poprawiono wciecia w metodzie

--- src/ZadLista.java
+++ src/ZadLista.java
@@ -40,20 +40,20 @@ public class ZadLista extends JPanel {
 	
 	//do listy w programie
 	public DefaultListModel<Object> lista_zadania() {
-			DefaultListModel<Object>lista = new DefaultListModel<Object>();
-			String czy_wykonane;
-			try {
-				for(int i = 0; i < zadania.size(); i++) {
-					if(zadania.get(i).pobierz_czy_wykonane()) { czy_wykonane = "*"; }
-					else { czy_wykonane = " "; }
-					
-					lista.addElement(czy_wykonane + "   " + zadania.get(i).pobierz_data_zadanie().toString() + "   " + zadania.get(i).pobierz_tytul_zadanie());
-				}
-			} catch (IndexOutOfBoundsException e) {
-				e.printStackTrace();
-				return null;
-			}
+		DefaultListModel<Object>lista = new DefaultListModel<Object>();
+		String czy_wykonane;
+		try {
+			for(int i = 0; i < zadania.size(); i++) {
+				if(zadania.get(i).pobierz_czy_wykonane()) { czy_wykonane = "*"; }
+				else { czy_wykonane = " "; }
 				
-			return lista;
+				lista.addElement(czy_wykonane + "   " + zadania.get(i).pobierz_data_zadanie().toString() + "   " + zadania.get(i).pobierz_tytul_zadanie());
+			}
+		} catch (IndexOutOfBoundsException e) {
+			e.printStackTrace();
+			return null;
+		}
+				
+		return lista;
 	}
 }
