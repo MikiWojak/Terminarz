61c887c3371ceac7ad5c73134cfde8da05e6808e
MikiWojak <mikizarowa8@gmail.com>
2020-02-07 11:35:21 +0100

    Funkcja do generowania listy
    Dodano funkcje do przygotowania obiektow do listy
    Funkcja wyswietlenia listy zadan nie jest jeszcze gotowa

--- src/Program.java
+++ src/Program.java
@@ -10,6 +10,8 @@ import model.Zadanie;
 import model.Zlozone;
 
 import java.awt.Font;
+
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.sql.Date;
@@ -121,5 +123,28 @@ public class Program {
 		warstwy.add(panel);
 		warstwy.repaint();
 		warstwy.revalidate();
+	}
+	
+	public void lista() {
+		
+	}
+	
+	//do listy w programie
+	public DefaultListModel<Object> lista_zadania() {
+		DefaultListModel<Object>lista = new DefaultListModel<Object>();
+		String czy_wykonane;
+		try {
+			for(int i = 0; i < zadania.size(); i++) {
+				if(zadania.get(i).pobierz_czy_wykonane()) { czy_wykonane = "*"; }
+				else { czy_wykonane = " "; }
+					
+				lista.addElement(czy_wykonane + "   " + zadania.get(i).pobierz_data_zadanie().toString() + "   " + zadania.get(i).pobierz_tytul_zadanie());
+			}
+		} catch (IndexOutOfBoundsException e) {
+			e.printStackTrace();
+			return null;
+		}
+				
+		return lista;
 	}
 }
