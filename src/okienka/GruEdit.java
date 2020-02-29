15e113f475ff4fdcb56236f2536729787feacf9d
MikiWojak <mikizarowa8@gmail.com>
2020-02-29 17:34:43 +0100

    Edycja grupy - okienko
    Przygotowanie elementow okienka GruEdit.

--- src/okienka/GruEdit.java
+++ src/okienka/GruEdit.java
@@ -8,11 +8,22 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class GruEdit extends JDialog {
 	private boolean czy_modyfikacja;
 	
 	private JPanel panel;
+	private JTextPane tresc_nazwa;
+	private JLabel opis_nazwa;
+	private JLabel opis_opis;
+	private JButton btn_anuluj;
+	private JButton btn_mod;
+	private JTextPane tresc_opis;
+	private JLabel tytul;
 	//dodanie grupy
 	public GruEdit() {
 		initComp();
@@ -23,13 +34,53 @@ public class GruEdit extends JDialog {
 	
 	//elementy w okienku
 	private void initComp() {
-		setBounds(100, 100, 650, 300);
+		setBounds(100, 100, 725, 325);
 		getContentPane().setLayout(null);
 		
 		panel = new JPanel();
-		panel.setBounds(0, 0, 632, 253);
+		panel.setBounds(0, 0, 706, 278);
 		getContentPane().add(panel);
 		panel.setLayout(null);
+		
+		tresc_nazwa = new JTextPane();
+		tresc_nazwa.setFont(new Font("Arial", Font.PLAIN, 20));
+		tresc_nazwa.setEditable(false);
+		tresc_nazwa.setBounds(118, 56, 578, 30);
+		panel.add(tresc_nazwa);
+		
+		opis_nazwa = new JLabel("Nazwa");
+		opis_nazwa.setHorizontalAlignment(SwingConstants.RIGHT);
+		opis_nazwa.setFont(new Font("Arial", Font.ITALIC, 20));
+		opis_nazwa.setBounds(12, 56, 94, 30);
+		panel.add(opis_nazwa);
+		
+		opis_opis = new JLabel("Opis");
+		opis_opis.setHorizontalAlignment(SwingConstants.RIGHT);
+		opis_opis.setFont(new Font("Arial", Font.ITALIC, 20));
+		opis_opis.setBounds(12, 94, 94, 120);
+		panel.add(opis_opis);
+		
+		tresc_opis = new JTextPane();
+		tresc_opis.setFont(new Font("Arial", Font.PLAIN, 20));
+		tresc_opis.setEditable(false);
+		tresc_opis.setBounds(118, 94, 578, 120);
+		panel.add(tresc_opis);
+		
+		btn_anuluj = new JButton("Anuluj");
+		btn_anuluj.setFont(new Font("Arial", Font.BOLD, 20));
+		btn_anuluj.setBounds(596, 227, 100, 39);
+		panel.add(btn_anuluj);
+		
+		btn_mod = new JButton("Dodaj");
+		btn_mod.setFont(new Font("Arial", Font.BOLD, 20));
+		btn_mod.setBounds(449, 227, 135, 39);
+		panel.add(btn_mod);
+		
+		tytul = new JLabel("Dodaj zadanie");
+		tytul.setHorizontalAlignment(SwingConstants.CENTER);
+		tytul.setFont(new Font("Arial", Font.BOLD, 20));
+		tytul.setBounds(12, 13, 682, 30);
+		panel.add(tytul);
 	}
 	
 	//koniec inicjowania okienka
