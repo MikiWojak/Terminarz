b71b0d273605f89bc04055d822473981faeb084c
MikiWojak <mikizarowa8@gmail.com>
2020-03-10 19:36:58 +0100

    GruPrzypis - nowe elementy
    Dodano kolejny tytul i pola na tekst.
    Upewnienie uzytkownika, ze operuje na wlasciwym zadaniu.
    Poczatek prac nad wygladem interfejsu usuwania grupy z zadania.

--- src/okienka/GruPrzypis.java
+++ src/okienka/GruPrzypis.java
@@ -22,48 +22,90 @@ import java.awt.Choice;
 public class GruPrzypis extends JDialog {
 	private Zadanie zadanie;
 	private List<Grupa>grupy;
+	private boolean czyUsuwanie;
 	
 	private JButton btnDodaj;
 	private JButton btnAnuluj;
-	private JLabel lblNewLabel;
+	private JLabel tytulDodaj;
 	private Choice choice;
+	private JLabel tytulZad;
+	private JLabel trescTytul;
+	private JLabel trescData;
 	
+	//dodawanie grupy do zadania
 	public GruPrzypis(Zadanie zadanie) {
 		initComp();
 		setTitle("Dodaj grupê");
+		
+		this.zadanie = zadanie;
+		czyUsuwanie = false;
+		
+		initFinal();
+	}
+	
+	//usuwanie grupy z zadania
+	public GruPrzypis(Zadanie zadanie, boolean czyUsuwanie) {
+		initComp();
+		usuwanieWyglad();
+		
+		this.zadanie = zadanie;
+		this.czyUsuwanie = czyUsuwanie;
 		
 		initFinal();
 	}
 	
 	private void initComp() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 700, 325);
 		getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 432, 253);
+		panel.setBounds(0, 0, 682, 278);
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		btnDodaj = new JButton("Dodaj");
 		btnDodaj.setFont(new Font("Arial", Font.BOLD, 20));
-		btnDodaj.setBounds(214, 210, 100, 30);
+		btnDodaj.setBounds(461, 235, 100, 30);
 		panel.add(btnDodaj);
 		
 		btnAnuluj = new JButton("Anuluj");
 		btnAnuluj.setFont(new Font("Arial", Font.BOLD, 20));
-		btnAnuluj.setBounds(323, 210, 100, 30);
+		btnAnuluj.setBounds(570, 235, 100, 30);
 		panel.add(btnAnuluj);
 		
-		lblNewLabel = new JLabel("Dodaj grup\u0119 do zadania");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 20));
-		lblNewLabel.setBounds(12, 13, 408, 30);
-		panel.add(lblNewLabel);
+		tytulDodaj = new JLabel("Dodaj grup\u0119 do zadania");
+		tytulDodaj.setHorizontalAlignment(SwingConstants.CENTER);
+		tytulDodaj.setFont(new Font("Arial", Font.BOLD, 20));
+		tytulDodaj.setBounds(12, 13, 658, 30);
+		panel.add(tytulDodaj);
 		
 		choice = new Choice();
 		choice.setFont(new Font("Arial", Font.BOLD, 20));
-		choice.setBounds(12, 49, 408, 30);
+		choice.setBounds(12, 49, 658, 30);
 		panel.add(choice);
+		
+		tytulZad = new JLabel("Zadanie w skr\u00F3cie");
+		tytulZad.setHorizontalAlignment(SwingConstants.CENTER);
+		tytulZad.setFont(new Font("Arial", Font.BOLD, 20));
+		tytulZad.setBounds(12, 84, 658, 30);
+		panel.add(tytulZad);
+		
+		trescTytul = new JLabel("Tytu\u0142");
+		trescTytul.setHorizontalAlignment(SwingConstants.CENTER);
+		trescTytul.setFont(new Font("Arial", Font.PLAIN, 20));
+		trescTytul.setBounds(12, 127, 658, 30);
+		panel.add(trescTytul);
+		
+		trescData = new JLabel("Data");
+		trescData.setHorizontalAlignment(SwingConstants.CENTER);
+		trescData.setFont(new Font("Arial", Font.PLAIN, 20));
+		trescData.setBounds(12, 170, 658, 30);
+		panel.add(trescData);
+	}
+	
+	//dla usuwania zadania
+	private void usuwanieWyglad() {
+		setTitle("Usuñ grupê");
 	}
 	
 	//koniec inicjowania okienka
