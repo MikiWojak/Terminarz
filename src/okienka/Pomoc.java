663b39eab314d634a540f841a4a1174c06e49145
MikiWojak <mikizarowa8@gmail.com>
2020-03-23 16:17:43 +0100

    Pomoc - kolejne artykuly

--- src/okienka/Pomoc.java
+++ src/okienka/Pomoc.java
@@ -50,7 +50,7 @@ public class Pomoc extends JDialog {
 	
 	private void initComp() {
 		//wymiary programu
-		int program_szerokosc = 800;
+		int program_szerokosc = 1000;
 		int program_wysokosc = 600;
 		int system_szerokosc = Toolkit.getDefaultToolkit().getScreenSize().width;
 		int system_wysokosc = Toolkit.getDefaultToolkit().getScreenSize().height;
@@ -65,14 +65,14 @@ public class Pomoc extends JDialog {
 		setTitle("Pomoc");
 		
 		panel = new JPanel();
-		panel.setBounds(0, 0, 782, 553);
+		panel.setBounds(0, 0, 982, 553);
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		tytul_artykulu = new JLabel("Tytu\u0142 artyku\u0142u");
 		tytul_artykulu.setHorizontalAlignment(SwingConstants.CENTER);
 		tytul_artykulu.setFont(new Font("Arial", Font.BOLD, 20));
-		tytul_artykulu.setBounds(324, 13, 446, 30);
+		tytul_artykulu.setBounds(424, 12, 546, 30);
 		panel.add(tytul_artykulu);
 		
 		btn_zamknij = new JButton("Zamknij");
@@ -82,13 +82,13 @@ public class Pomoc extends JDialog {
 			}
 		});
 		btn_zamknij.setFont(new Font("Arial", Font.BOLD, 20));
-		btn_zamknij.setBounds(630, 510, 140, 30);
+		btn_zamknij.setBounds(830, 510, 140, 30);
 		panel.add(btn_zamknij);
 		
 		scroll_tematy = new JScrollPane();
 		scroll_tematy.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scroll_tematy.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		scroll_tematy.setBounds(12, 13, 300, 527);
+		scroll_tematy.setBounds(12, 13, 400, 527);
 		panel.add(scroll_tematy);
 		
 		tematy = new JList();
@@ -105,7 +105,7 @@ public class Pomoc extends JDialog {
 		scroll_tresc = new JScrollPane();
 		scroll_tresc.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scroll_tresc.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		scroll_tresc.setBounds(324, 58, 446, 439);
+		scroll_tresc.setBounds(424, 58, 546, 439);
 		panel.add(scroll_tresc);
 		
 		tresc_artykulu = new JTextPane();
@@ -122,14 +122,21 @@ public class Pomoc extends JDialog {
 	
 	//lista tematów
 	private void utworz_liste_tematow() {
-		lista_tematow.add("Wstêp");
-		lista_tematow.add("Widok");
-		lista_tematow.add("Zadanie");
-		lista_tematow.add("Pomoc");
-		lista_tematow.add("Lista zadañ");
-		lista_tematow.add("Filtr grup");
-		lista_tematow.add("Szczegó³y zadania");
-		lista_tematow.add("Oznacz jako zrobione/niezrobione");
+		lista_tematow.add("Wstêp");											//0
+		lista_tematow.add("Widok");											//1
+		lista_tematow.add("Zadanie");										//2
+		lista_tematow.add("Pomoc");											//3
+		lista_tematow.add("Lista zadañ");									//4
+		lista_tematow.add("Filtr grup");									//5
+		lista_tematow.add("Szczegó³y zadania");								//6
+		lista_tematow.add("Oznacz jako zrobione/niezrobione");				//7
+		lista_tematow.add("Dodawanie, modyfikacja i usuwanie zadañ");		//8
+		lista_tematow.add("Przypisywanie zadañ do grup");					//9
+		lista_tematow.add("Lista grup");									//10
+		lista_tematow.add("Szczegó³y grupy");								//11
+		lista_tematow.add("Dodawanie, modyfikacja i usuwanie grup");		//12
+		lista_tematow.add("Plik z danymi");									//13
+		lista_tematow.add("Rozwi¹zywanie problemów");						//14
 		
 		tematy.setModel(rekordy_tematy());
 	}
@@ -155,14 +162,15 @@ public class Pomoc extends JDialog {
 		switch(index) {
 		case 0:		//Wstêp
 			return "Terminarz"
-					+ "\n\nTytu³: Miko³aj ¯arnowski";
+					+ "\n\nTytu³: Miko³aj ¯arnowski"
+					+ "\n\nProgram ma za zadanie u³atwiæ zarz¹dzanie zadaniami, planami, spotkaniami, itp.";
 		case 1:		//Widok
-			return "Menu znajduj¹ce siê na górnej belce. Umo¿liwia prze³¹czenie siê pomiêdzy list¹ zadañ a list¹ grup."
+			return "Znajduje siê w menu na górnej belce. Umo¿liwia prze³¹czenie siê pomiêdzy list¹ zadañ a list¹ grup."
 					+ "\n\nOpcje:"
 					+ "\n- Zadania;"
 					+ "\n- Grupy.";
 		case 2:		//Zadanie
-			return "Menu znajduj¹ce siê na górnej belce. S¹ tam operacje na zadnaiach. Jest w³¹czone tylko gdy jest w³¹czona lista zadañ."
+			return "Znajduje siê w menu na górnej belce. S¹ tam operacje na zadnaiach. Jest w³¹czone tylko gdy jest w³¹czona lista zadañ."
 					+ "\n\nOpcje:"
 					+ "\n- Dodaj;"
 					+ "\n- Modyfikuj;"
@@ -170,7 +178,7 @@ public class Pomoc extends JDialog {
 					+ "\n- Dodaj do grupy;"
 					+ "\n- Usuñ z grupy.";
 		case 3:		//Pomoc
-			return "";
+			return "Znajduje siê w menu na górnej belce. Uruchamia okno pomocy, w którym siê teraz znajdujesz.";
 		case 4:		//Lista zadañ
 			return "Na liœcie zadañ prezentowane s¹ takie informacje:"
 					+ "\n- czy zadanie jest zrobione (* lub jej brak);"
@@ -189,8 +197,25 @@ public class Pomoc extends JDialog {
 					+ "\n- priorytet zadania.";
 		case 7:		//Oznacz jako zrobione/niezrobione
 			return "Ten przycisk znajduje siê pod list¹ zadañ po prawej stronie. Pozwala na szybkie oznaczenie niezrobionego zadania jako zrobione i na odwrót - zrobionego jako niezrobione.";
-		case 8:
-			return "";
+		case 8:		//Dodawanie, modyfikacja i usuwanie zadañ
+			return "Zadanie -> Dodaj"
+					+ "\nUzupe³niasz pola danymi. Wprowadzasz datê, u¿ywaj¹c odpowiednio: roku, miesi¹ca i dnia. Zapisujesz nowe zadanie."
+					+ "\n\nZadanie -> Modyfikuj"
+					+ "\nWybierasz jedno zadanie. Modyfikujesz b¹dŸ poprawiasz wybrane zadanie. Potwierdzasz wprowadzone zmiany."
+					+ "\n\nZadanie -> Usuñ"
+					+ "\nWybierasz jedno zadanie. Potwierdzasz chêæ usuniêcia zadania.";
+		case 9:		//Przypisywanie zadañ do grup
+			return "Przypisywanie zadañ do grup";
+		case 10:	//Lista grup
+			return "Lista grup";
+		case 11:	//Szczegó³y grupy
+			return "Szczegó³y grupy";
+		case 12:	//Dodawanie, modyfikacja i usuwanie grup
+			return "Dodawanie, modyfikacja i usuwanie grup";
+		case 13:	//Plik z danymi
+			return "Plik z danymi";
+		case 14:	//Rozwi¹zywanie problemów
+			return "Rozwi¹zywanie problemów";
 		default:
 			return "";
 		}
